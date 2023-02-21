5c8759b1cc918cfcb675178f9f307596f7096c19
TatianaSherstyuk2022 <grinkevichtatiana@gmail.com>
2023-02-21 20:43:11 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,22 +2,22 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { fetchCast } from 'services/api';
 
-export function* Cast() {
-  const [loading, setLoading] = useState(false);
+export function Cast() {
+  const [isLoading, setIsLoading] = useState(false);
   const { movieId } = useSearchParams();
   const [actors, setActors] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading(true);
+        setIsLoading(true);
 
         const response = await fetchCast(movieId);
         setActors(response);
       } catch (error) {
         console.log(error.message);
       } finally {
-        setLoading(false);
+        setIsLoading(false);
       }
     };
     fetchData(movieId);
